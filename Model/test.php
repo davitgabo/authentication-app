2d7d0f15148da2c492fc72238d5491d3ff37769a
Davit Gabashvili <dgabashvili@gmail.com>
2022-01-10 23:05:45 +0400

    delete test

--- Model/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "/home/davitgabashvili/Projects/lesson1/Portal_OOP/Model/Users.php";
-
-$uri = "Model/test.php";
-
-$b = strpos( $_SERVER['REQUEST_URI'], trim($uri,".php") );
-echo $b;
