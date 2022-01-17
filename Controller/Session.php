71d86a5ddede7300fd20929a6f00ec5ef17515c9
Davit Gabashvili <dgabashvili@gmail.com>
2022-01-17 23:58:30 +0400

    Add '/' to redirect function url
    there was a bug redirecting from User/edit/{ID},
    to logout, it redirected like this User/edit/logout,
    so i added '/' to every redirect function.

--- Controller/Session.php
+++ Controller/Session.php
@@ -13,27 +13,27 @@ class Session{
         if ($uri=="login") {
             if (isset($_COOKIE['ID']) && $_COOKIE['ID'] == session_id()) {
                 if ($_COOKIE['Role'] == "authenticated") {
-                    header("location: home");
+                    header("location: /home");
                 } else {
-                    header("location: adminpanel");
+                    header("location: /adminpanel");
                 }
             }
         }  elseif ($uri=="adminpanel"){
                 if (isset($_COOKIE['ID']) && $_COOKIE['ID'] == session_id()) {
                     if ($_COOKIE['Role'] != "administrator" && $_COOKIE['Role'] != "manager") {
-                        header("location: home");
+                        header("location: /home");
                     }
                 } else {
                     session_destroy();
-                    header("location: login");
+                    header("location: /login");
                 }
         }  else {
                 if ( isset($_COOKIE['ID']) ) {
                     if ($_COOKIE['ID'] != session_id()) {
-                        header("location: login");
+                        header("location: /login");
                     }
                 } else {
-                    header("location: login");
+                    header("location: /login");
                 }
         }
     }
